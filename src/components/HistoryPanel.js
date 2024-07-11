0e7143f5d3ef24891deb113db6ab2cd7286dde5e
AyushKhamar <ayushkhamar8@gmail.com>
2024-07-11 15:02:15 +0530

    feat: history panel implementation

--- src/components/HistoryPanel.js
+++ src/components/HistoryPanel.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./HistoryPanel.css";
 
 const HistoryPanel = ({ children }) => {
-  return <></>;
+  const handleClearHistory = () => {
+    localStorage.removeItem("calculatorHistory");
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <div className="history-header">
+        <h3>History</h3>
+      </div>
+      <ul className="history-area">{children}</ul>
+      <button className="clear-history" onClick={handleClearHistory}>
+        Clear History
+      </button>
+    </>
+  );
 };
 
 export default HistoryPanel;
